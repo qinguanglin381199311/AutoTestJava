91f34107dd04e08fd1407e68c86bcccfa10a084b
覃光林 <381199311@qq.com>
2018-07-27 14:47:07 +0800

    新增testNG注解 @test标签 @Befor After class  method测试 @套件测试 @忽略测试 @分组测试 @组件中的分组测试 @异常测试 @依赖测试 @参数化测试(文件参数化) @多线程测试 @超时测试

--- mybatis/src/main/java/com/cmp/controller/Demo.java
+++ mybatis/src/main/java/com/cmp/controller/Demo.java
@@ -1,4 +1,29 @@
-package com.cmp.model.controller;
+package com.cmp.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//@Log4j
+@RestController
+@RequestMapping("v1")
+@Api(value = "v1", description = "这我是第一个版本的demo")
 public class Demo {
+
+    @Autowired //起动机加载，也就是说启动这个类，当前方法就被加载了
+    private SqlSessionTemplate template;
+
+    @RequestMapping(value="/getUserCount",method = RequestMethod.GET)
+    @ApiOperation(value = "可以获取用户数",httpMethod = "GET")
+    public int getUserCount(){
+        return template.selectOne("getUserCounts");
+    }
+
+
+
 }
